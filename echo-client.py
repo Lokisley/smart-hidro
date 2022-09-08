850cd2d68c2626f6249bdf7075cd9ca74f4c3b86
Lokisley <lokisley@hotmail.com>
2022-09-08 17:11:18 -0300

    add time cycle and json notation on client

--- echo-client.py
+++ echo-client.py
@@ -1,12 +1,25 @@
 import socket
+import time
+import json
 
 HOST = "172.16.103.212" #localhost
 PORT = 54321
+cont = 0
+
+hidro = {
+    "id": 0,
+    "consumo": 200,
+    "vazao": 20
+    }
+
+jobj = json.dumps(hidro)
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.sendto("Hello World".encode('utf-8'), (HOST, PORT))
-    data, addr = s.recvfrom(1024)
+    while True:
+        s.sendto(f"{jobj} {cont}".encode('utf-8'), (HOST, PORT))   #envia info para servidor
+        data, addr = s.recvfrom(1024)                                   #recebe resposta
 
-print(f"Received {data.decode('utf-8')}")
+        print(f"Received {data.decode('utf-8')} {cont}")                #printa resposta
 
-s.close()
+        time.sleep(1)                                                   #seta delay
+        cont += 1
